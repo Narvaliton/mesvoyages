81d2f34db73e25a232610dd7feea49bcbac57352
Narvaliton <mxclin.pro@gmail.com>
2022-11-02 12:48:30 +0100

    Phase 8 : ORM (affichage en colonne des informations de la BDD)

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\VisiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,12 +14,21 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class VoyagesController extends AbstractController{
     
+    private $repository;
+   
+    public function __construct(VisiteRepository $repository){
+        $this->repository = $repository;
+    }
+    
     /**
      * @Route("/voyages", name="voyages")
      * @return Response
      */
     public function index(): Response{
-       return $this->render("pages/voyages.html.twig"); 
+        $visites = $this->repository->findAll();
+       return $this->render("pages/voyages.html.twig", [
+           'visites' => $visites
+       ]); 
 }
     
 }
--- src/Entity/Visite.php
+++ src/Entity/Visite.php
@@ -86,6 +86,15 @@ class Visite
         return $this->datecreation;
     }
 
+    public function getDatecreationString(): string
+    {
+        if ($this->datecreation == null){
+            return "";
+        }else{
+            return $this->datecreation->format("d/m/Y");
+        }
+    }
+    
     public function setDatecreation(?\DateTimeInterface $datecreation): self
     {
         $this->datecreation = $datecreation;
