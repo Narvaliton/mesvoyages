8f44ea6e6b6af4808d3bd247b6dde83d9ad3b436
Narvaliton <mxclin.pro@gmail.com>
2022-11-03 15:49:50 +0100

    Phase 19: Gestion des mails

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request; 
+use App\Entity\Contact;
+use App\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
+/**
+ * Description of ContactController
+ *
+ * @author maxco
+ */
+class ContactController extends AbstractController{
+    /**
+     * @Route("/contact", name="contact")
+     * @return Response
+     */
+    public function index(Request $request, MailerInterface $mailer): Response{
+        $contact = new Contact();
+        $formContact = $this->createForm(ContactType::class, $contact);
+        $formContact->handleRequest($request);
+        
+        if($formContact->isSubmitted() && $formContact->isValid()){
+            $this->sendMail($mailer, $contact);
+            $this->addFlash('success', 'Message envoyé');
+            return $this->redirectToRoute('contact');
+        }
+        return $this->render("pages/contact.html.twig", [
+            'contact' => $contact,
+            'formcontact' => $formContact->createView()
+        ]);
+    }
+    
+    public function sendMail(MailerInterface $mailer, Contact $contact)
+    {
+        $email = (new Email())
+            ->from('hello@example.com')
+            ->to('contact@mesvoyages.com')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Message du site de voyages')
+            ->html($this->renderView(
+                    'pages/_email.html.twig', [
+                        'contact' =>$contact
+                    ]
+                ),
+                'utf-8'
+            );
+
+        $mailer->send($email);
+
+        // ...
+    }
+}
